a11afbc3ea7c7a51be242c3bae823b8710d22e7b
micro155 <qawe2006@naver.com>
2021-11-15 20:53:38 +0900

    2021/11/15 - 코드랩 컴포즈 프로젝트 수정

--- week 1-Jetpack Compose basics/week1CodesLabCompose/app/src/main/java/com/ssafy/week1_codeslabcompose/MainActivity.kt
+++ week 1-Jetpack Compose basics/week1CodesLabCompose/app/src/main/java/com/ssafy/week1_codeslabcompose/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,6 +40,8 @@ private fun MyApp(names: List<String> = listOf("World", "Compose")) {
 
 @Composable
 private fun Greeting(name: String) {
+//    var expanded = false
+    var expanded = remember { mutableStateOf(false)}
     Surface (
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
@@ -47,8 +51,8 @@ private fun Greeting(name: String) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
-            OutlinedButton(onClick = { /*TODO*/ }) {
-                Text("Show more")
+            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+                Text(if (expanded.value) "Show less" else "Show more")
             }
         }
     }
